64c72b4a4bde33767b3f1d85a5278e2fa0c8c022
Tal Amitai <talamitai32@gmail.com>
2021-02-24 21:02:05 -0500

    javascript-variables

--- app.js
+++ app.js
@@ -4,7 +4,31 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Tal";
+console.log(firstName);
+
+let lastName = "Amitai";
+console.log(lastName);
+
+var age = 99;
+console.log(age);
+
 
 // Exercise 2
 
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+//how to do template literal?
+
+
 // Exercise 3
+
+const city = "Culver City"
+const pastime = "playing sports and traveling"
+
+let myStory = "Hi, my name is " + fullName + "! " + "I am from " + city + ". " + "My favorite things to do are " + pastime + ".";
+console.log(myStory)
+
+let myStory1 = `\Hi, my name is ${fullName}! I am from ${city}. My favorite things to do are ${pastime}.`;
+console.log(myStory1)
